b1bd7d9b546cb7426859b7eb7b3ba682c83cbd84
unknown <denismiklos6@gmail.com>
2024-06-09 15:28:14 +0300

    Added CRUD functionality

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const e = require('express');
 
 const app = express();
 
+// Body parser
+app.use(express.json())
+
 //  Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));

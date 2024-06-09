94578a4f8aa78dd2152e4d83d9b52f8936d0b770
unknown <denismiklos6@gmail.com>
2024-06-09 16:47:05 +0300

    Refactored controllers and middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcamSchema = new mongoose.Schema({
     name: {
@@ -100,6 +101,12 @@ const BootcamSchema = new mongoose.Schema({
     {
       toJSON: { virtuals: true },
       toObject: { virtuals: true }
+});
+
+// Create bootcam slug from the name
+BootcamSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcamSchema)

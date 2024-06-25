79426d2f6e77b7a871d84018d89b710f8889e47a
unknown <denismiklos6@gmail.com>
2024-06-25 19:36:26 +0300

    Added authorization

--- routes/courses.js
+++ routes/courses.js
@@ -12,6 +12,8 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect, authorize } = require('../middleware/auth');
+
 router
   .route('/')
   .get(
@@ -21,7 +23,7 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
 router
   .route('/:id')
   .get(
@@ -31,7 +33,7 @@ router
     }),
     getCourses
   )
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
